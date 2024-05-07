eef011271ddf090038aa2e3421a8023a8fa40052
valdirsillva <valdirpiresba@gmail.com>
2024-05-07 16:49:50 -0300

    chore: corrigindo nome DepartmentController

---
+++ src/main/java/com/br/edu/app/controllers/DepartmentController.java
@@ -0,0 +1,42 @@
+package com.br.edu.app.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.edu.app.domain.department.Department;
+import com.br.edu.app.dtos.DepartmentRequestDTO;
+import com.br.edu.app.services.DepartmentService;
+
+@RestController
+@RequestMapping("/api")
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @GetMapping("/departamentos")
+    public ResponseEntity<List<DepartmentRequestDTO>> getDepartments() {
+        List<Department> departmentsList = this.departmentService.getDepartments();
+
+        List<DepartmentRequestDTO> departmentDTOs = departmentsList.stream()
+            .map(department -> new DepartmentRequestDTO(department.getId(), department.getName(), department.getCreatedAt()))
+            .collect(Collectors.toList());
+
+        return ResponseEntity.ok(departmentDTOs);
+    }
+
+    @PostMapping("/departamentos")
+    public ResponseEntity<Department> createDepartment(@RequestBody DepartmentRequestDTO body) {
+        Department department = this.departmentService.create(body);
+
+        return ResponseEntity.created(null).body(department);
+    }
+}
